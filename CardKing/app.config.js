b952d60c66fc7d67b867b9f1ce4269f73457fc20
Sgg67 <sageyanoff@gmail.com>
2026-03-18 21:36:42 -0400

    fix the path

--- CardKing/app.config.js
+++ CardKing/app.config.js
@@ -10,7 +10,7 @@ export default {
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
     splash: {
-      image: "./assets/splash.png",
+      image: "./assets/images/splash.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff"
     },
