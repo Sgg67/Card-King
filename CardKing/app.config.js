85952bd080991f1780b491f8ce456e59eca416ad
Sgg67 <sageyanoff@gmail.com>
2026-03-18 21:37:32 -0400

    fix the config file

--- CardKing/app.config.js
+++ CardKing/app.config.js
@@ -10,7 +10,7 @@ export default {
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
     splash: {
-      image: "./assets/images/splash.png",
+      image: "./assets/images/splash-icon.png",
       resizeMode: "contain",
       backgroundColor: "#ffffff"
     },
